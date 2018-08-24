1fc0553a1647e1ca79b3a0ae1241f0ba440ad900
drevitsky <4227723@gmail.com>
2018-08-24 11:20:27 +0300

    first commit

---
+++ src/js/bootstrap.js
@@ -0,0 +1,12 @@
+/// = ./alert.js
+/// = ./button.js
+/// = ./carousel.js
+/// = ./collapse.js
+/// = ./dropdown.js
+/// = ./tab.js
+/// = ./scrollspy.js
+/// = ./tooltip.js
+/// = ./popover.js
+/// = ./util.js
+/// = ./modal.js
+//= ./scripts.js
